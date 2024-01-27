adeceac446e5a89711ff07082fe822ca4dab1f07
karishmaaaaa <karishma.ss@kalvium.community>
2024-01-27 15:57:40 +0530

    haa

---
+++ forms/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react'
+import Forms from './Components/Forms'
+
+function App() {
+
+  return (
+    <>
+      <Forms />
+    </>
+  )
+}
+
+export default App
